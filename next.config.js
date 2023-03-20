ded568f6918c75222d9aedbda9ac2e7a1e52ade7
Nate Jahnke <naterased@gmail.com>
2023-03-20 09:05:51 -0500

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    REACT_APP_SUBSCRIPTION_KEY: process.env.REACT_APP_SUBSCRIPTION_KEY,
-    REACT_APP_ENDPOINT: process.env.REACT_APP_ENDPOINT,
+    NEXT_PUBLIC_SUBSCRIPTION_KEY: process.env.NEXT_PUBLIC_SUBSCRIPTION_KEY,
+    NEXT_PUBLIC_ENDPOINT: process.env.NEXT_PUBLIC_ENDPOINT,
   },
 };
