0d754b26f58cadf8437744dce059df8b492f47a6
Nate Jahnke <naterased@gmail.com>
2023-03-30 15:31:02 -0500

    added part of navbar.

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,12 @@
+import Navbar from "./Navbar";
+
 const Layout = ({ children }) => {
-  return <div className="content">{children}</div>;
+  return (
+    <div className="content">
+      <Navbar />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
--- pages/browse.js
+++ pages/browse.js
@@ -1,31 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Head from "next/head";
 import { db } from "../firebase";
 
 const Browse = ({ animals }) => {
-  console.log("Fetched animals:", animals);
   return (
     <div className="min-h-screen bg-slate-50">
       <Head>
         <title>Browse Animals</title>
       </Head>
-      <div className="container px-4 py-8 mx-auto">
+      <div className="px-4 py-8">
         <h1 className="mb-6 text-3xl font-bold text-center">Browse Animals</h1>
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid gap-6 mx-auto w-[300px] sm:grid-cols-2 sm:w-auto md:grid-cols-3 lg:grid-cols-4">
           {animals.map((animal) => (
             <div
               key={animal.id}
-              className="relative max-w-[300px] overflow-hidden bg-white rounded-md shadow-md"
+              className="relative max-w-[300px] bg-white shadow-md"
             >
-              <Image
-                src={animal.imageURL}
-                alt={animal.name}
-                width={300}
-                height={200}
-                className={`mx-auto transition duration-500 ease-in-out transform hover:scale-110`}
-              />
-              <div className="absolute bottom-0 left-0 w-full py-1 text-lg font-semibold text-white text-center bg-black bg-opacity-70">
+              <div className="relative w-full h-0 pb-[66.666%]">
+                <Image
+                  src={animal.imageURL}
+                  alt={animal.name}
+                  fill
+                  className={`mx-auto object-cover rounded-lg transition duration-500 ease-in-out transform hover:scale-110`}
+                />
+              </div>
+              <div className="absolute bottom-0 left-0 w-full py-1 text-lg font-semibold text-center text-white bg-black rounded-lg bg-opacity-70">
                 {animal.name}
               </div>
             </div>
@@ -43,7 +43,7 @@ export async function getServerSideProps() {
     return {
       id: doc.id,
       ...data,
-      timestamp: data.timestamp.toDate().toISOString(), // Convert Firestore timestamp to ISO string
+      timestamp: data.timestamp.toDate().toISOString(),
     };
   });
   console.log("Animals fetched in getServerSideProps:", animals);
